0071f8db99c71efcddfa0f4817895a49c8144eaa
naaz11 <naazlashkari11@gmail.com>
2023-01-17 17:31:42 -0700

    upload code

---
+++ src/app/question/question.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { QuestionService } from '../service/question.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  public name: string = "";
+  public questionList: any = [];
+  public currentQuestion: number = 0;
+  public marks: number = 0;
+  counter = 60;
+  correctAnswer: number = 0;
+  inCorrectAnswer: number = 0;
+  interval$: any;
+  progress: string = "0";
+  isQuizCompleted : boolean = false;
+  viewResult: boolean = false;
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
+    this.startCounter();
+  }
+  getAllQuestions() {
+    this.questionService.getQuestionJson()
+      .subscribe(res => {
+        this.questionList = res.questions;
+      })
+  }
+  nextQuestion() {
+    if (this.currentQuestion !== this.questionList.length-1){
+      this.currentQuestion++;
+      this.resetCounter();
+      this.getProgressPercent();
+    }
+    else {
+      this.isQuizCompleted = true;
+      this.stopCounter();
+    }
+  }
+  previousQuestion() {
+    this.currentQuestion--;
+  }
+  answer(currentQno: number, option: any) {
+
+    if(currentQno === this.questionList.length){
+      this.isQuizCompleted = true;
+      this.stopCounter();
+    }
+    if (option.correct) {
+      this.marks += 10;
+      this.correctAnswer++;
+      setTimeout(() => {
+        this.currentQuestion++;
+        this.resetCounter();
+        this.getProgressPercent();
+      }, 1000);
+
+
+    } else {
+      setTimeout(() => {
+        this.currentQuestion++;
+        this.inCorrectAnswer++;
+        this.resetCounter();
+        this.getProgressPercent();
+      }, 1000);
+
+      this.marks -= 10;
+    }
+  }
+  startCounter() {
+    this.interval$ = interval(1000)
+      .subscribe(val => {
+        this.counter--;
+        if (this.counter === 0) {
+          if (this.currentQuestion !== this.questionList.length-1){
+            this.currentQuestion++;
+            this.counter = 60;
+            this.marks -= 10;  
+          } else {
+            this.isQuizCompleted = true;
+            this.stopCounter();
+          }
+        }
+      });
+    setTimeout(() => {
+      this.interval$.unsubscribe();
+    }, 600000);
+  }
+  stopCounter() {
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+  resetCounter() {
+    this.stopCounter();
+    this.counter = 60;
+    this.startCounter();
+  }
+  resetQuiz() {
+    this.resetCounter();
+    this.getAllQuestions();
+    this.marks = 0;
+    this.counter = 60;
+    this.currentQuestion = 0;
+    this.progress = "0";
+
+  }
+  getProgressPercent() {
+    this.progress = ((this.currentQuestion / this.questionList.length) * 100).toString();
+    return this.progress;
+
+  }
+
+  reviewResults(){
+    this.viewResult = true;
+  }
+}
